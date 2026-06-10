675943fb0bb213a4dd8244be74713c683ff58c3d
miniv12-dot <152743804+miniv12-dot@users.noreply.github.com>
2026-06-10 16:23:52 +0200

    Configure Django monolith production settings and WSGI profile

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-bope@c*i1d-+*suq-#6_i%e%^x!$^3604=kos3i6r%0jso-o+7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -121,7 +122,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_FILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # config/settings.py
 
@@ -132,3 +135,4 @@ REST_FRAMEWORK = {
     )
 }
 CORS_ALLOW_ALL_ORIGINS = True
+WSGI_APPLICATION = 'FORGE.wsgi.application'
